bdc34740949cc45a3fa1f6cf59448b5aecfb64b0
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-12 12:47:29 +0300

    Rename variables and Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,10 @@
 public class CreditPaymentService {
 
-    public int calculate (double amount_rub, double duration_year, double rate) {
-        double ratePerMonth = rate/(100*12);
-        double k = (ratePerMonth * (Math.pow ((1 + ratePerMonth), (duration_year * 12)))) / ((Math.pow ((1 + ratePerMonth), (duration_year * 12))) - 1);
-        double payment = amount_rub * k;
+    public int calculate(double amountRub, double durationYear, double rate) {
+        double ratePerMonth = rate / (100 * 12);
+        double k = (ratePerMonth * (Math.pow((1 + ratePerMonth), (durationYear * 12))))
+                / ((Math.pow((1 + ratePerMonth), (durationYear * 12))) - 1);
+        double payment = amountRub * k;
         return (int) payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.println("Ежемесячный платеж на срок 1 год:");
         System.out.println(service.calculate(1_000_000, 1, 9.99));
 
-
         System.out.println();
         System.out.println("Ежемесячный платеж на срок 2 года:");
         System.out.println(service.calculate(1_000_000, 2, 9.99));
